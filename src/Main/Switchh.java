ac06fc588a0a7502440cb3a108fc12c5d1a8a734
mi-szmudzinska <miszmudzinska@gmail.com>
2019-11-25 01:11:17 +0100

    Change swing app to console

---
+++ src/Main/Switchh.java
@@ -0,0 +1,32 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Switchh {
+
+    public Switchh(){
+        switch(number){
+            case 1:
+                System.out.println("***************************************");
+                System.out.println("Wybrana figura KWADRAT");
+                System.out.println("***************************************");
+                System.out.println("Podaj długość boku:    [cm]");
+//                Scanner bok = new Scanner(System.in);
+//                String a = bok.nextLine();
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+
+
+
+        }
+    }
+    private int number;
+
+
+}
